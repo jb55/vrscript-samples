e900f36eec97a9cd0d21c21cc0ee45920deb8532
William Casarin <bill@casarin.me>
2015-11-16 22:29:06 -0800

    spin: vrscript 0.2

--- spin.rkt
+++ spin.rkt
@@ -1,28 +1,43 @@
-#!r6rs
-(import (only (racket base) require))
+#lang racket/base
 (require "remote.rkt")
 ; Everything up to this mark will be stripped and replaced
 ; for the embedded version.
 ; %%%END-OF-HEADER%%%
 ;----------------------------------------------------------------------------------
 
+(define *tex* 0)
+
+(define textures
+  '("file:///sdcard/temp/basketball.tga"
+    "file:///sdcard/temp/volleyball.tga"
+    "file:///sdcard/temp/baseball.tga"))
+
 ;-----------------
 ; tic function
 ;-----------------
 (define (tic)
   ; select a background
-  (cmd-pano! "http://s3.amazonaws.com/o.oculuscdn.com/v/test/social/avatars/office_lobby.JPG")
+  (+pano "http://s3.amazonaws.com/o.oculuscdn.com/v/test/social/avatars/office_lobby.JPG")
+
+  ; user swapping of texture
+  (if (pressed-action)
+      (set! *tex* (remainder (+ 1 *tex*) 3))
+      #f)
   
   ; spin a model in front of the view
-  (cmd-model! "http://s3.amazonaws.com/o.oculuscdn.com/v/test/social/avatars/head-0.ovrscene"
+;  (cmd-model! "https://s3.amazonaws.com/o.oculuscdn.com/netasset/ball.ovrscene"
+  (+model "http://s3.amazonaws.com/o.oculuscdn.com/v/test/social/avatars/head-0.ovrscene"
               (mat4-compose (mat4-rotate-y (input-time *input*))
-                            (mat4-translate 0.0 1.7 -1.0)))
+                            (mat4-translate 0.0 0.0 -1.0))
+;              (opt-texture (list-ref textures *tex*))
+              )
   
   ; add a text label 
   )
 
 ; This connects to the HMD over TCP when run from DrRacket, and is ignored when embedded.
-; Replace the IP address with the value shown on the phone when NetHmd is run.
+; Replace the IP address with the value shown on the phone when vrscript is run.
 ; The init function is optional, use #f if not defined.
 (remote "172.22.52.94" #f tic)
+;(remote "172.22.52.248" #f tic)
  
