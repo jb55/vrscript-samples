5848987febf8e2fc125daa63d904cf2e4f0de28b
William Casarin <bill@casarin.me>
2015-11-16 22:35:16 -0800

    voice-around: ovr-vrscript-public

--- voice-around.rkt
+++ voice-around.rkt
@@ -5,7 +5,7 @@
 ; %%%END-OF-HEADER%%%
 ;----------------------------------------------------------------------------------
 
-(uri WAV-VOICE-TEST  "http://s3.amazonaws.com/o.oculuscdn.com/netasset/wav/mono_human_voice_test_01b.wav")
+(uri WAV-VOICE-TEST  "http://vr.oculuscdn.com/assets/sounds/test/mono_human_voice_test_01b.wav")
 
 (define NAME-VOICE 1)
 
@@ -14,14 +14,14 @@
 ;-----------------
 (define (tic)
   (define xform (mat4-compose (mat4-rotate-y pi)
-                              (mat4-translate 0.0 0.0 -1.0)
+                              (mat4-translate 0.0 0.0 2.0)
                               (mat4-rotate-y (input-time *input*))))
   
   ; select a background
-  (+pano "http://s3.amazonaws.com/o.oculuscdn.com/v/test/social/avatars/office_lobby.JPG")
+  (+pano "http://vr.oculuscdn.com/assets/panos/office_demo.JPG")
 
   ; spin a model in front of the view
-  (+model "http://s3.amazonaws.com/o.oculuscdn.com/v/test/social/avatars/head-0.ovrscene"
+  (+model "http://vr.oculuscdn.com/assets/models/icon_speaker.ovrscene"
               xform)
 
   ; update the audio position
@@ -34,5 +34,5 @@
 ; This connects to the HMD over TCP when run from DrRacket, and is ignored when embedded.
 ; Replace the IP address with the value shown on the phone when vrscript is run.
 ; The init function is optional, use #f if not defined.
-(remote "172.22.52.94" #f tic)
+(remote "172.22.52.204" #f tic)
  
